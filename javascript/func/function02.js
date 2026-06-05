821d8027c1b08afdc375cd3dbf79d952cbd8173d
jws909 <jws090909@gmail.com>
2026-06-05 15:48:46 +0900

    javascript 함수, 콜백함수

---
+++ javascript/func/function02.js
@@ -0,0 +1,83 @@
+
+
+// 함수 리턴
+
+function sum1() {
+    console.log('sum1()');
+    return 100;
+}
+
+sum1();
+let s1 = sum1();
+console.log(s1);
+console.log(sum1());
+
+const sum2 = function(a, b) {
+    return a + b;
+}
+
+console.log(sum2(30, 50));
+
+const sum3 = function sum333(a, b) {
+    return a+b;
+}
+console.log(sum3(10, 20));
+
+const sum4 = (x, y) => {
+    return x + y;
+}
+console.log(sum4(100, 200));
+
+// 화살표 함수, => 뒤에 괄호없이 바로 나오는 값을 return 값으로 인식
+const sum5 = (x, y) => x + y;  //return x+y;
+console.log(sum5(1000, 2000));
+
+const sum6 = ()=>1+2+3+4+5;
+console.log(sum6());
+
+
+//-----------------------
+
+function func(){
+    let x = 10;
+}
+
+if(1==1){
+    let x = 40;
+}
+
+{
+    let x = 50;
+}
+
+let x = 11;
+
+console.log(x);
+
+
+//호이스팅
+console.log(w);
+w = 20;
+var w = 30;
+
+r = 3000;
+var r;
+console.log(r);
+
+
+//함수 호이스팅
+
+//function check(){}
+
+check();
+
+function check(){
+    console.log('check()');
+}
+
+
+// 일회용 함수  즉시사용
+
+(function ontime(){
+    console.log('일회용 함수 실행');
+})();
---
+++ javascript/func/function04.js
@@ -0,0 +1,79 @@
+//function
+
+function arrForEach1(){
+    let arr = [10,20,30,40];
+
+    for(let i in arr){
+        console.log(i + " : " + arr[i]);
+    }
+}
+arrForEach1();
+
+function arrForEach2(arr){
+    for(let i in arr){
+        console.log(i + " : " + arr[i]);
+    }
+}
+arrForEach2([1,2,3,4]);
+
+function arrForEach3(func){
+    let arr = [11,20,30,40,55];
+    func(arr);
+}
+arrForEach3(arrForEach2);
+
+
+
+
+//----------------------------
+
+const printFunc = (index, value)=> {
+    console.log(index + " : " + value);
+}
+
+function arrForEach4(func){
+    let arr = [666,777,868,939,121];
+
+    for(let i in arr){ //i : index
+        func(i, arr[i]);
+    }
+}
+arrForEach4(printFunc);
+
+
+function arrForEach5(arr, func){
+    for(let i in arr){
+        func(i, arr[i]);
+    }
+}
+arrForEach5([9,3,4,2,1], printFunc);
+
+arrForEach5([11,22,33,44,55], (i, v)=>{
+    console.log('index:'+ i + " <<<------->>> values:" + v);
+});
+
+//---------------------------------------
+// 콜백함수를 매개변수로 필요로하는 기능
+// setTimeout(함수, 시간) -> 매개변수로 넘긴 시간이 지난 후 함수를 실행
+
+setTimeout(()=>{
+    console.log('setTimeout 에 매개변수로 넘긴 콜백함수');
+}, 3000);  // 시간단위 ms     1000 -> 1초
+
+const cb = ()=>{
+    console.log("cb() 함수 호출");
+}
+
+const outCb = (f)=>{
+    console.log('outCb() 함수가 수행하는 코드 1');
+    console.log('outCb() 함수가 수행하는 코드 2');
+    console.log('outCb() 함수가 수행하는 코드 3');
+    f();
+}
+
+outCb(cb);
+
+setTimeout(()=>{
+    console.log('setTimeout 콜백함수 실행');
+    outCb(cb);
+}, 5000); // 5초
